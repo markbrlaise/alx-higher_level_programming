cf6ae5131f1cf5f2457c97b88d7eee4f4934f5c5
markbrlaise <markkiguli10@gmail.com>
2022-12-07 05:15:13 -0800

    Fix checker

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        if search == replace:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
+    new_list = my_list.copy()
+    count = 0
+    if not my_list:
+        return ()
+    for i in new_list:
+        if search == i:
+            new_list[count] = replace
+        count = count + 1
+    return (new_list)
