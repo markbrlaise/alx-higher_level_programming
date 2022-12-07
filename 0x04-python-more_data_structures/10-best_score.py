c9e33af9cbfc9eccb0cb4cc8a0022a5d3bffeba9
markbrlaise <markkiguli10@gmail.com>
2022-12-07 06:43:57 -0800

    fixed my bad code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        max_v = max(a_dictionary.values())
-        for i in a_dictionary:
-            if a_dictionary[i] == max_v:
-                return a_dictionary[i]
-    else:
-        return ()
+    if not a_dictionary:
+        return (None)
+
+    return max(a_dictionary, key=a_dictionary.get)
