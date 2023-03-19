79bf8e09fcefe45ac7174505a947ba4de192da81
markbrlaise <markkiguli10@gmail.com>
2023-03-19 06:50:47 +0000

    fixed typo

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ close the instances then
 import MySQLdb
 import sys
 
-if name("__main__"):
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
