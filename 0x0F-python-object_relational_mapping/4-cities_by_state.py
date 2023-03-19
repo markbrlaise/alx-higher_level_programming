94b7a55468d60212349918a160d23b147a23f560
markbrlaise <markkiguli10@gmail.com>
2023-03-19 07:04:24 +0000

    fixed style

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-import sys, MySQLdb
+import sys
+import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2]
-	db=sys.argv[3], port=3306)
-cur = db.cursor()
-cur.execute("select * from cities ")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("select * from cities ")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
 
