a71046cb588c1bd94d17a76089a303edc0555549
markbrlaise <markkiguli10@gmail.com>
2023-03-19 06:59:21 +0000

    fixed style and header

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
+"""
 
-#basic protection against a sql injection
+basic protection against a sql injection
+"""
 
-import sys, MySQLdb
+import sys
+import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-match = sys.argv[4]
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (match, ))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    match = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
