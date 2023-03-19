87d128e2d8bfa8b70527c73687ca1ecd9a74feaa
markbrlaise <markkiguli10@gmail.com>
2023-03-19 06:36:11 +0000

    fixed style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""
 
-#connect to a db with MySQLdb
-#get a cursor object 
-#run the sql command then
-#get rows and print them then
-#close the cursor and mysql instances
+connect to a db with MySQLdb
+get a cursor object
+run the sql command then
+get rows and print them then
+close the cursor and mysql instances
+"""
 
-import MySQLdb, sys
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
@@ -19,4 +22,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-
