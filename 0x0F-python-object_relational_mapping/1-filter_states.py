7d2b5450e8270aaabe15461969e5fa34e2aca8b8
markbrlaise <markkiguli10@gmail.com>
2023-03-19 06:39:32 +0000

    fixed style and header

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
 
-#create a MySQLdb instance and connect
-#create a curosr instance from MySQLdb instance and run the SQL query then get 
-#rows
-#close the instances then
+create a MySQLdb instance and connect
+create a curosr instance from MySQLdb instance and run the SQL query
+then get rows
+close the instances then
+"""
+import MySQLdb
+import sys
 
-import MySQLdb, sys
-
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], 
-        db=sys.argv[3], port=3306)
+db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3], port=3306)
 cur = db.cursor()
 cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
 rows = cur.fetchall()
