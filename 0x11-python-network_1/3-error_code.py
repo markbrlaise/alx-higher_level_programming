a511f7e97cbfbc0f3a6ecf2075b8b6724e983bc2
markbrlaise <markkiguli10@gmail.com>
2023-04-03 03:03:23 +0000

    fixed typo

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         with request.urlopen(url) as response:
             print(response.read().decode("utf-8"))
     except error.HTTPError as er:
-        print("Error: {}".format(er.code))
+        print("Error code: {}".format(er.code))
